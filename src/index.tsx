a31e8f48f0c292de665c9394db668686d9c91423
Ruslan-Baldeznik <100410676+Ruslan-Baldeznik@users.noreply.github.com>
2024-01-27 17:10:43 +0400

    some chenges...

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
-import App from './App/App';
-import reportWebVitals from './reportWebVitals';
+import App from './app/App';
 
 import store from './store';
 import { Provider } from 'react-redux';
@@ -22,4 +21,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
